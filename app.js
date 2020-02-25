b50dba70d69c6500623fb97410cabf73684e8cba
ejaramillom <ejaramilo@hotmail.com>
2020-02-24 21:54:59 -0500

    added list in pug functionality

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
-// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mydatabase', { useNewUrlParser: true });
+const bodyParser = require("body-parser");
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mydb', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 // definimos el schema
 const schema = new mongoose.Schema({
@@ -11,26 +12,30 @@ const schema = new mongoose.Schema({
 });
 // definimos el modelo
 const Visitor = mongoose.model("Visitor", schema);
-const visitorTable = {};
-
+// definir las rutas app
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   let name = req.query.name;
   if (!name || name.length === 0) {
     name = "Anónimo";
-  }
-
-  Visitor.create({ name: name }, function(err) {
-    if (err) return console.error(err);
-  });
-
-  Visitor.find(function(err, visitors){
-    visitors.forEach(function(visitor){
-      visitorTable[visitor] = visitor;
+    count = 1;
+    Visitor.create({ name: name, count: count }, function(err) {
+      if (err) return console.error(err);
     });
-    console.log(visitorTable);
+  }
+  Visitor.updateOne({ name: name}, { count: count + 1 } );
+  Visitor.find(function(err, visitors){
+    const visitorCount = [];
+    visitors.forEach(function(visitor){
+      visitorCount.push(visitor);
+    });
+    console.log(visitorCount);
+    res.render('visitorTable', {
+     visitorCount: visitorCount
+    });
   });
-
-  res.send (visitorTable);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
