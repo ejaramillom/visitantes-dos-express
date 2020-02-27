5c6391277a416aff71b46f54ea485e2784220572
ejaramillom <ejaramilo@hotmail.com>
2020-02-27 00:00:43 -0500

    modified conditionals

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mydatabase', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/dbtwo', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 // definimos el schema
 const schema = new mongoose.Schema({
@@ -19,38 +19,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   let name = req.query.name;
   let count = 1;
-  let existVisitor = Visitor.countDocuments({ name: name }, function(err, value) {
-    if (err) return handleError(err);
-    console.log("Hay " + value + " " + name);
+  let existVisitor = Visitor.findOne({ name: name }, function(err, value) {
+    if (err) return console.error(err);
+    console.log( "El visitante " + value + " existe" );
   });
 
   if (!name || name.length === 0) {
     name = "Anónimo";
-    count = 1;
     Visitor.create({ name: name, count: count }, function(err) {
       if (err) return console.error(err);
     });
-  } else if (name.length > 0) {
+  } else if ( Visitor.findOne({ name: name }) != null ) {
+    Visitor.findOne({ name: name}, function(err, article) {
+      if (err) return console.error(err);
+      console.log(article.count);
+      article.count = article.count + 1;
+      article.save(function(err) {
+        if (err) return console.error(err);
+      });
+    });
+  } else {
     Visitor.create({ name: name, count: count }, function(err) {
       if (err) return console.error(err);
     });
-  } else if (existVisitor > 0) {
-    Visitor.count += 1;
-    // this doesnt work but for the sake of testing
-    Visitor.updateOne({ name: name}, { count: count } );
   }
 
-  Visitor.find(function(err, visitors){
+  Visitor.find( function(err, visitors){
     let visitorCount = [];
-    visitors.forEach(function(visitor){
+    visitors.forEach( function(visitor){
       visitorCount.push(visitor);
     });
-    console.log(visitorCount);
-    res.render('visitorTable', {
+    console.log( visitorCount );
+    res.render( 'visitorTable', {
      visitorCount: visitorCount
     });
   });
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
+
 
