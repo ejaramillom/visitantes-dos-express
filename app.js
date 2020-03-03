0e46ceac84cd2ec308686fa6efc10e135523c624
ejaramillom <ejaramilo@hotmail.com>
2020-03-03 10:19:42 -0500

    Updated full app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/dbsix", { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 // definimos el schema
 const schema = new mongoose.Schema({
@@ -13,45 +13,55 @@ const schema = new mongoose.Schema({
 // definimos el modelo
 const Visitor = mongoose.model("Visitor", schema);
 // definir las rutas app
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set("view engine", "pug");
+app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+
   let name = req.query.name;
   let count = 1;
-  let existVisitor = [];
+  let existVisitor =[];
 
-  Visitor.findOne({ name: name }, function( err, value ) {
-    if (err) return console.error( err );
-    existVisitor.push( name );
-
-      if (!name || name.length === 0) {
-        name = "Anónimo";
-        Visitor.create({ name: name, count: count }, function( err ) {
-          if (err) return console.error( err );
-        });
-      } else if ( value == null )  {
-        Visitor.create({ name: name, count: count }, function( err ) {
-          if (err) return console.error( err );
-        });
-      }
-      else {
-        value.count = value.count + 1;
-        value.save(function(err) {
-          if (err) return console.error(err);
-        });
-      }
-  });
-
-  Visitor.find(function(err, visitors){
+  Visitor.find( function(err, visitors){
     let visitorCount = [];
-    visitors.forEach(function(visitor){
+    visitors.forEach( function(visitor){
       visitorCount.push(visitor);
     });
-    res.render('visitorTable', {
-     visitorCount: visitorCount
-    });
+    if ( visitorCount.length == 0){
+      visitorCount = [{ id: "", name: "", count: ""}];
+      res.render( "visitorTable", {
+       visitorCount: visitorCount
+      });
+    } else {
+      res.render( "visitorTable", {
+       visitorCount: visitorCount
+      });
+    }
   });
+
+  Visitor.findOne({ name: name }, function( err, value ) {
+     if (err) return console.error( err );
+     existVisitor.push( name );
+
+       if (!name || name.length === 0) {
+         name = "Anónimo";
+         Visitor.create({ name: name, count: count }, function( err ) {
+           if (err) return console.error( err );
+         });
+         res.render( "visitorTable" );
+       } else if ( value == null )  {
+         Visitor.create({ name: name, count: count }, function( err ) {
+           if (err) return console.error( err );
+         });
+         res.render( "visitorTable" );
+       } else {
+         value.count = value.count + 1;
+         value.save(function(err) {
+           if (err) return console.error(err);
+         });
+         res.render( "visitorTable" );
+       }
+   });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log("Listening on port 3000!"));
