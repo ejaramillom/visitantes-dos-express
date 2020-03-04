6795e4eeaabed08e295b9d724e3dcf5a4bce899e
ejaramillom <ejaramilo@hotmail.com>
2020-03-03 19:16:48 -0500

    full code refactor

--- app.js
+++ app.js
@@ -1,67 +1,51 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require( "express" );
+const mongoose = require( "mongoose" );
+const bodyParser = require( "body-parser" );
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/mongo-1", { useNewUrlParser: true });
-mongoose.connection.on("error", function(e) { console.error(e); });
+mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost:27017/mongo-1", { useNewUrlParser: true });
+mongoose.connection.on( "error", function(e) { console.error(e); });
 // definimos el schema
 const schema = new mongoose.Schema({
-  count: Number,
-  name: String
+  name: String,
+  count: Number
 });
 // definimos el modelo
-const Visitor = mongoose.model("Visitor", schema);
+const Visitor = mongoose.model( "Visitor", schema );
 // definir las rutas app
-app.set("view engine", "pug");
-app.set("views", "views");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-
+app.set( "view engine", "pug" );
+app.set( "views", "views" );
+app.use( bodyParser.urlencoded({ extended: false }));
+app.get( "/", async ( req, res ) => {
   let name = req.query.name;
-  let count = 1;
-  let existVisitor =[];
-
-  Visitor.find( function(err, visitors){
-    let visitorCount = [];
-    visitors.forEach( function(visitor){
-      visitorCount.push(visitor);
+  let visitorCount = [];
+  let value = await Visitor.findOne({ name: name });
+  let showVisitors = async function() {
+    await Visitor.find(function(err, visitors){
+      visitors.forEach(function(visitor){
+        visitorCount.push(visitor);
+      });
     });
-    if ( visitorCount.length == 0){
-      visitorCount = [{ id: "", name: "", count: ""}];
-      res.render( "visitorTable", {
-       visitorCount: visitorCount
-      });
-    } else {
-      res.render( "visitorTable", {
-       visitorCount: visitorCount
-      });
-    }
-  });
+    res.render("visitorTable", {
+     visitorCount: visitorCount
+    });
+  };
+  
+  if (!name || name.length === 0) {
+    visitor = new Visitor({ name: "Anónimo", count: 1 });
+    visitor.save();
+    showVisitors();
+  } else if ( value == null )  {
+    visitor = new Visitor({ name: name, count: 1 });
+    visitor.save();
+    showVisitors();
+  } else {
+    value.count = value.count + 1;
+    value.save();
+    showVisitors();
+  }
 
-  Visitor.findOne({ name: name }, function( err, value ) {
-     if (err) return console.error( err );
-     existVisitor.push( name );
-
-       if (!name || name.length === 0) {
-         name = "Anónimo";
-         Visitor.create({ name: name, count: count }, function( err ) {
-           if (err) return console.error( err );
-         });
-         res.render( "visitorTable" );
-       } else if ( value == null )  {
-         Visitor.create({ name: name, count: count }, function( err ) {
-           if (err) return console.error( err );
-         });
-         res.render( "visitorTable" );
-       } else {
-         value.count = value.count + 1;
-         value.save(function(err) {
-           if (err) return console.error(err);
-         });
-         res.render( "visitorTable" );
-       }
-   });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
+
